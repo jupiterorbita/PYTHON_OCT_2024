acc4837e868873ebb6a619e751bc7c25c7a5c1f7
JupiterOrbita <38020888+jupiterorbita@users.noreply.github.com>
2024-10-02 15:28:47 -0600

    add w2d3 finish CRUD except delete

--- W2D3_CRUD/dog_model.py
+++ W2D3_CRUD/dog_model.py
@@ -30,3 +30,46 @@ class Dog:
                 this_dog_instance = cls(row)
                 dog_instances.append(this_dog_instance)
         return dog_instances
+    
+    # --- CREATE ----
+    @classmethod
+    def create(cls, data):
+        query = """
+                INSERT INTO dogs
+                    (name, age, breed, color)
+                VALUES
+                    (%(name)s, %(age)s, %(breed)s, %(color)s);
+                """
+        result = connect_to_mysql('dogs_db').query_db(query, data)
+        return result
+
+    # ------ READ ONE -----
+    @classmethod
+    def get_one(cls, id):
+        data = {
+            'id' : id
+        }
+        query = """
+            SELECT * FROM dogs
+            WHERE id = %(id)s;
+        """
+        result = connect_to_mysql('dogs_db').query_db(query, data)
+        print("########## get_one -->> result:", result) # [ {dict} ]
+
+        this_one_dog_instance = cls(result[0])
+        return this_one_dog_instance # {Dog Object / instance}
+    
+
+    # ---- UPDATE -----
+    @classmethod
+    def update(cls, data):
+        query = """
+        UPDATE dogs
+        SET
+            name = %(name)s,
+            age = %(age)s,
+            breed = %(breed)s,
+            color = %(color)s
+        WHERE id = %(id)s;
+        """
+        return connect_to_mysql('dogs_db').query_db(query, data)
